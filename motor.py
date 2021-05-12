f9bf5201a7acfe8e5073e5a075ee5eb0c238f9d5
multifeb13 <musicfair@gmail.com>
2021-05-12 13:22:57 +0900

    feat: control rotate speed by volume

--- motor.py
+++ motor.py
@@ -1,6 +1,6 @@
 import machine
 import utime
-from machine import PWM, Pin
+from machine import ADC, PWM, Pin
 
 pinMotorInA = machine.Pin(16, machine.Pin.OUT)
 pinMotorInB = machine.Pin(17, machine.Pin.OUT)
@@ -10,8 +10,26 @@ pwmMotorInA.freq( 1000 )
 pwmMotorInB = PWM( pinMotorInB )
 pwmMotorInB.freq( 1000 )
 
+adc = ADC(Pin(27))		#Pin27 = ADC1
+
+def getADCRatio():
+	data = adc.read_u16()
+
+	if data < 384:
+		data = 0
+
+	return data / 65535
+
+def getDuty():
+	return int(65535 * getADCRatio())
+
 while True:
-	#rotate CW
-	pwmMotorInA.duty_u16( 32767 )	# 100% = 65535, 0% = 0
-	pwmMotorInB.duty_u16( 0 )
+	duty = getDuty()
+
+	if duty == 0:	#state:break
+		pwmMotorInA.duty_u16( 0 )
+		pwmMotorInB.duty_u16( 0 )
+	else:			#state:drive(CW)
+		pwmMotorInA.duty_u16( duty )
+		pwmMotorInB.duty_u16( 0 )
 	utime.sleep(1)
