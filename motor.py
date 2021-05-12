f70f416fe0f4d13e7e319bb6fc002f0f1ce88bfc
multifeb13 <musicfair@gmail.com>
2021-05-12 11:32:05 +0900

    feat: rotate motor with PWM

--- motor.py
+++ motor.py
@@ -1,11 +1,17 @@
 import machine
 import utime
+from machine import PWM, Pin
 
 pinMotorInA = machine.Pin(16, machine.Pin.OUT)
 pinMotorInB = machine.Pin(17, machine.Pin.OUT)
 
+pwmMotorInA = PWM( pinMotorInA )
+pwmMotorInA.freq( 1000 )
+pwmMotorInB = PWM( pinMotorInB )
+pwmMotorInB.freq( 1000 )
+
 while True:
 	#rotate CW
-	pinMotorInA.value(1)
-	pinMotorInB.value(0)
+	pwmMotorInA.duty_u16( 32767 )	# 100% = 65535, 0% = 0
+	pwmMotorInB.duty_u16( 0 )
 	utime.sleep(1)
